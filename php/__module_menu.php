d50cf2bcefa5c61da480a3c9de7fe1fd5a69d163
Tsjippy <ewaldha@hotmail.com>
2024-11-20 09:12:22 +0100

    changed: removed anonymous functions

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -7,7 +7,8 @@ DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
 DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
 
-add_filter('sim_submenu_options', function($optionsHtml, $moduleSlug, $settings, $moduleName){
+add_filter('sim_submenu_options', __NAMESPACE__.'\moduleOptions', 10, 4);
+function moduleOptions($optionsHtml, $moduleSlug, $settings, $moduleName){
 	//module slug should be the same as grandparent folder name
 	if($moduleSlug != MODULE_SLUG){
 		return $optionsHtml;
@@ -20,4 +21,4 @@ add_filter('sim_submenu_options', function($optionsHtml, $moduleSlug, $settings,
 	<?php
 
 	return ob_get_clean();
-}, 10, 4);
+}
