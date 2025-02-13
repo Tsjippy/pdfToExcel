c8cb70932493b03d37c8c6d064654c8a15628be5
Tsjippy <ewaldha@hotmail.com>
2025-02-13 09:27:54 +0100

    changed: module hooks now include module slug

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -6,19 +6,3 @@ const MODULE_VERSION		= '8.0.3';
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
 DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
-
-add_filter('sim_submenu_options', __NAMESPACE__.'\moduleOptions', 10, 4);
-function moduleOptions($optionsHtml, $moduleSlug, $settings, $moduleName){
-	//module slug should be the same as grandparent folder name
-	if($moduleSlug != MODULE_SLUG){
-		return $optionsHtml;
-	}
-
-	ob_start();
-	
-    ?>
-
-	<?php
-
-	return ob_get_clean();
-}
